f7c8aeb124b38029ede23d007ecba3e5cd5ab214
poorlittlekid <makstv90@gmail.com>
2020-09-16 18:01:19 +0500

    Update main.php

--- project/views/main.php
+++ project/views/main.php
@@ -3,8 +3,6 @@
 require_once 'template.php';
 
 $content = '<div id="header">
-                <h2>api.luckru.ru</h2><br>
-                <a href="client.addForm">Добавить клиента</a>    
             <div id="auth_block">';
 
 
